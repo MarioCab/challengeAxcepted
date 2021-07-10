4ef22a442599590aade3d6772bd4c836a15e1da9
Frank McDermott <fmcdermott4@gmail.com>
2021-07-10 13:12:58 -0400

    creating mongodb connection

--- server/server.js
+++ server/server.js
@@ -1,13 +1,17 @@
+const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+
 const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+// const db = require('./config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-const server = new ApolloServer({
-  typeDefs,
-  resolvers,
-});
+// const server = new ApolloServer({
+//   typeDefs,
+//   resolvers,
+// });
 
 server.applyMiddleware({ app });
 
@@ -22,9 +26,9 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-db.once('open', () => {
-  app.listen(PORT, () => {
-    console.log(`API server running on port ${PORT}!`);
-    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-  });
-});
+// db.once('open', () => {
+//   app.listen(PORT, () => {
+//     console.log(`API server running on port ${PORT}!`);
+//     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+//   });
+// });
