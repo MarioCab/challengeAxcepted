bbbed024370ffeff67134238c2188a16a4331a97
Frank McDermott <fmcdermott4@gmail.com>
2021-07-24 11:31:16 -0400

    challenge world form updated

--- client/src/components/ChallengeWorld/index.js
+++ client/src/components/ChallengeWorld/index.js
@@ -1,61 +1,89 @@
-import React, { useState, Component  } from "react";
+import React, { useState } from "react";
 import {Form, Row, Col, Button} from "react-bootstrap";
-import DatePicker from "react-datepicker";
-import "react-datepicker/dist/react-datepicker.css";
-import Timekeeper from 'react-timekeeper';
+import {ADD_POST} from '../../utils/mutations';
+import { useMutation } from '@apollo/client';
 
 
 const ChallengeWorld = () =>{
-    const [startDate, setStartDate] = useState(new Date());
-    
-    
+    const [formState, setFormState] = useState({
+        title: '',
+        body: '',
+        location: '',
+    });
+    const [addPost, { error, data }] = useMutation(ADD_POST);
+    if(error){
+        console.log(error)
+    }
+    // update state based on form input changes
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        setFormState({
+            ...formState,
+            [name]: value,
+        });
+    };
+
+    // submit form
+    const handleFormSubmit = async (event) => {
+        event.preventDefault();
+        console.log(formState);
+
+        try {
+            const { data } = await addPost({
+            variables: { ...formState },
+            });
+        } catch {
+        }
+    }
     return(
         <div>
-            <Form>
-                <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
-                <Timekeeper />
-                <Row className="mb-3">
-                    <Form.Group as={Col} controlId="formGridEmail">
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+            <Form onSubmit={handleFormSubmit}>
+            <Col md={{ span: 6, offset: 3 }}>
+                    <Form.Group >
+                        <Form.Label> 
+                            What is your challenge?
+                        </Form.Label>
+                        <input
+                            className="form-input"
+                            placeholder="Enter challenge title here"
+                            name="title"
+                            type="text"
+                            value={formState.title}
+                            onChange={handleChange}
+                        />
+                        <Form.Label> 
+                            What are the details of your challenge?
+                        </Form.Label>
+                        <input
+                            className="form-input"
+                            placeholder="What do potential accepters need to know about this challenge?"
+                            name="body"
+                            type="text"
+                            value={formState.body}
+                            onChange={handleChange}
+                        />                        
+                        <Form.Label> 
+                            Where is this challenge taking place?
+                        </Form.Label>
+                        <input
+                            className="form-input"
+                            placeholder="Please enter location of challenge"
+                            name="location"
+                            type="text"
+                            value={formState.location}
+                            onChange={handleChange}
+                        />  
                     </Form.Group>
-
-                    <Form.Group as={Col} controlId="formGridPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
-                    </Form.Group>
-                </Row>
-
-                <Form.Group className="mb-3" controlId="formGridAddress1">
-                    <Form.Label>Address</Form.Label>
-                    <Form.Control placeholder="1234 Main St" />
-                </Form.Group>
-
-                <Form.Group className="mb-3" controlId="formGridAddress2">
-                    <Form.Label>Address 2</Form.Label>
-                    <Form.Control placeholder="Apartment, studio, or floor" />
-                </Form.Group>
-
-                <Row className="mb-3">
-                    <Form.Group as={Col} controlId="formGridCity">
-                    <Form.Label>City</Form.Label>
-                    <Form.Control />
-                    </Form.Group>
-
-                    <Form.Group as={Col} controlId="formGridZip">
-                    <Form.Label>Zip</Form.Label>
-                    <Form.Control />
-                    </Form.Group>
-                </Row>
-
-                <Form.Group className="mb-3" id="formGridCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button> 
-            </Form>
+                    <button
+                        className="btn btn-block btn-info"
+                        style={{ cursor: 'pointer' }}
+                        type="submit"
+                    >
+                        Submit
+                    </button>
+                </Col>                
+            </Form>            
         </div>
     );
 };
