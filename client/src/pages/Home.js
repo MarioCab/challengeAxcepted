21cda2f2abe1612e4eb476c4388e0452e1ede920
Printice Lawton <printicelaw10@gmail.com>
2021-07-24 13:22:10 -0400

    still works

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,30 +1,30 @@
 import React from "react";
 
-import { useQuery } from "@apollo/client";
+// import { useQuery } from "@apollo/client";
 
-import PostList from "../components/Footer";
+// import PostList from "../components/Footer";
 
-import { QUERY_POSTS } from "../utils/queries";
+// import { QUERY_POSTS } from "../utils/queries";
 
 import Carousel from '../components/Carousel';
 
 import Hero from "../components/Hero";
 
 const Home = () => {
-  const { loading, data } = useQuery(QUERY_POSTS);
-  const posts = data?.posts || [];
+  // const { loading, data } = useQuery(QUERY_POSTS);
+  // const posts = data?.posts || [];
 
   return (
     <main>
        
       <Hero/>  
       <Carousel/> 
-      <div className="col-12 col-md-8 mb-3">
-        {loading ? <div>Loading...</div> : <PostList posts={posts} />}
+      {/* <div className="col-12 col-md-8 mb-3"> */}
+        {/* {loading ? <div>Loading...</div> : <PostList posts={posts} />} */}
 
             
        
-      </div>
+      {/* </div> */}
     </main>
   );
 };
--- client/src/pages/Mychallenges.js
+++ client/src/pages/Mychallenges.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React from "react";
+
+import { useQuery } from "@apollo/client";
+
+import PostList from "../components/Footer";
+
+import { QUERY_POSTS } from "../utils/queries";
+
+import Carousel from '../components/Carousel';
+
+import Hero from "../components/Hero";
 
 const Mychallenges = () => {
-return(
-    <h1>My Challenges (dashboard)</h1>
-)
+  const { loading, data } = useQuery(QUERY_POSTS);
+  const posts = data?.posts || [];
+
+  return (
+    <main>
+       
+      {/* <Hero/>   */}
+      {/* <Carousel/>  */}
+      <div className="col-12 col-md-8 mb-3">
+        {loading ? <div>Loading...</div> : <PostList posts={posts} />}
+
+            
+       
+      </div>
+    </main>
+  );
 };
 
 export default Mychallenges;
