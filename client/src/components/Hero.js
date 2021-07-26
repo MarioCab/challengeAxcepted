5d255e9920134c27cb5a580dcbf1c903f7bbc305
Mario Cabrera <yankeescab@gmail.com>
2021-07-25 23:31:42 -0400

    get started button now links to create challenge page

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,33 +1,42 @@
-import React from 'react'; 
-import Jumbotron from 'react-bootstrap/Jumbotron'; 
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button'
+import React from "react";
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import { Link } from "react-router-dom";
 
+function Hero(props) {
+  return (
+    <div>
+      <Jumbotron className="bg-transparent jumbotron-fluid p-0">
+        <Container fluid={true}>
+          <Row className="justify-content-center py-5">
+            <Col md={8} sm={12}>
+              <h1 className="display-3">
+                Are you ready to Axcept the Challenge?
+              </h1>
+              <p className="lead">
+                {" "}
+                If you thrive on challenges and hate to be outdone, you may want
+                to find some "friendly competition."{" "}
+              </p>
+              <hr className="my-2" />
+              <p>
+                Issue and axcept challenges in your community and around the
+                world!
+              </p>
+              <p className="lead">
+                <Link to="/challengeworld">
+                  <Button color="primary">Get Started</Button>
+                </Link>
+              </p>
+            </Col>
+          </Row>
+        </Container>
+      </Jumbotron>
+    </div>
+  );
+}
 
-function Hero (props) {
-      return (
-        <div>
-          <Jumbotron className="bg-transparent jumbotron-fluid p-0">
-              <Container fluid={true}>
-                  <Row className="justify-content-center py-5">
-                      <Col md={8} sm={12}>
-            <h1 className="display-3">Are you ready to Axcept the Challenge?</h1>
-            <p className="lead"> If you thrive on challenges and hate to be outdone, you may want to find some "friendly competition." </p>
-            <hr className="my-2" />
-            <p>Issue and axcept challenges in your community and around the world!</p>
-            <p className="lead">
-              <Button color="primary">Get Started</Button>
-            </p>
-                </Col>
-                </Row>
-            </Container>
-          </Jumbotron>
-        </div>
-      );
-    };
-
-
-export default Hero; 
-
+export default Hero;
