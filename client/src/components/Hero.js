1ab80398cad4fb3d5768e7cc22b6607b09fe30f0
Mario Cabrera <yankeescab@gmail.com>
2021-07-26 01:16:20 -0400

    working on makig getting started button redirect depending on being logged in or out

--- client/src/components/Hero.js
+++ client/src/components/Hero.js
@@ -1,12 +1,39 @@
 import React from "react";
+import { Redirect, useParams } from "react-router-dom";
 import Jumbotron from "react-bootstrap/Jumbotron";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Button from "react-bootstrap/Button";
-import { Link } from "react-router-dom";
+import Auth from "../utils/auth";
+import { useQuery } from "@apollo/client";
+import { QUERY_ME, QUERY_USER } from "../utils/queries";
 
-function Hero(props) {
+const Hero = () => {
+  const { username: userParam } = useParams();
+
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
+    variables: { username: userParam },
+  });
+
+  console.log(data);
+
+  const user = data?.me || data?.getUser || {};
+  console.log(user);
+
+  const handleRedirect = (event) => {
+    event.preventDefault();
+    if (Auth.getProfile().data.username === userParam) {
+      return <Redirect to="/challengeworld" />;
+    }
+    if (loading) {
+      return <h1>Loading...</h1>;
+    }
+    if (!user?.username) {
+      return <h1>Log in to post a challenge!</h1>;
+    } else console.log("dang she aint workin");
+  };
+
   return (
     <div>
       <Jumbotron className="bg-transparent jumbotron-fluid p-0">
@@ -27,9 +54,9 @@ function Hero(props) {
                 world!
               </p>
               <p className="lead">
-                <Link to="/challengeworld">
-                  <Button color="primary">Get Started</Button>
-                </Link>
+                <Button onClick={handleRedirect} color="primary">
+                  Get Started
+                </Button>
               </p>
             </Col>
           </Row>
@@ -37,6 +64,6 @@ function Hero(props) {
       </Jumbotron>
     </div>
   );
-}
+};
 
 export default Hero;
