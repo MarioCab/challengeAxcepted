7fb719708f4fb3429b949ee5561c6f1da590934d
Matthew Pawlak <pharaohnof@gmail.com>
2021-07-27 02:43:21 -0400

    centered everything on homepage....made posts display in reverse order, i.e. newest post displays at top

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -12,14 +12,17 @@ import Hero from "../components/Hero";
 
 const Home = () => {
   const { loading, data } = useQuery(QUERY_POSTS);
-  const posts = data?.posts || [];
+  // const posts = data?.posts || [];                    //normal post order
+  const posts = [...data?.posts].reverse() || [];        //reverse post order
 
   return (
-    <main>
+    <main className="text-center">
       <Hero />
       <Carousel />
-      <div className="col-12 col-md-8 mb-3">
+      <div className="row d-flex justify-content-center text-center">
+      <div className="col-12 col-md-8 mb-3 text-center">
         {loading ? <div>Loading...</div> : <PostList posts={posts} />}
+      </div>
       </div>
     </main>
   );
