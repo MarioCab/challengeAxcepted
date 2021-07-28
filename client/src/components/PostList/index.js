3d3ba1c54ab9d5f299dc437000a9a7b28e8a4029
Matthew Pawlak <pharaohnof@gmail.com>
2021-07-28 15:51:30 -0400

    added location to post card

--- client/src/components/PostList/index.js
+++ client/src/components/PostList/index.js
@@ -42,7 +42,9 @@ const PostList = ({ posts }) => {
             <div key={post._id} className="card mb-3">
               <h4 className="card-header bg-primary text-light p-2 m-0">
                 {post.title} <br />
-                <span style={{ fontSize: "1rem" }}>{post.body} </span>
+                <span style={{ fontSize: "1rem" }}>{post.body} </span><hr></hr>
+                <span style={{ fontSize: "1rem" }}>{post.location} </span>
+
               </h4>
               <div className="card-body bg-light p-2">
                 <p>{post.challenge}</p>
