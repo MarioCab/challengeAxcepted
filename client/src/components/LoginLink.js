d899d2cc0e4b78975d71d0c2805d845f91366b9a
Matthew Pawlak <pharaohnof@gmail.com>
2021-07-29 00:47:21 -0400

    refactored LoginLink.js and Login.js

--- client/src/components/LoginLink.js
+++ client/src/components/LoginLink.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
-// import { Link } from "react-router-dom";
 import { Nav, Modal, Tab, Button } from "react-bootstrap";
-import Auth from "../utils/auth"
+import Auth from "../utils/auth";
 
 import SignUpForm from "../components/SignupForm";
 import LoginForm from "../components/LoginForm";
@@ -12,80 +11,62 @@ const LoginLink = () => {
     Auth.logout();
   };
 
-  const [showModal, setShowModal] =
-    // true;
-    useState(false);
+  const [showModal, setShowModal] = useState(false);
 
   return (
-    
-
     <div>
-    {Auth.loggedIn() ? (
-      <>
-        <span className="text-success">{Auth.getProfile().data.username} LOGGED IN</span>
-        <Button 
-        variant="danger"
-        // className="btn btn-small btn-primary m-2" 
-        onClick={logout}>
-          Logout
-        </Button>
-      </>
-    ) : (
-      <>
-      {/* <h1>LOGIN</h1> */}
-      <Button variant="primary" onClick={() => setShowModal(true)}>
-        Login/Sign Up
-      </Button>
-      <Modal
-        size="lg"
-        show={showModal}
-        onHide={() => setShowModal(false)}
-        aria-labelledby="signup-modal"
-      >
-        {/* tab container to do either signup or login component */}
-        <Tab.Container defaultActiveKey="login">
-          <Modal.Header closeButton>
-            <Modal.Title id="signup-modal">
-              <Nav variant="pills">
-                <Nav.Item>
-                  <Nav.Link eventKey="login">Login</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey="signup">Sign Up</Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Tab.Content>
-              <Tab.Pane eventKey="login">
-                <LoginForm handleModalClose={() => setShowModal(false)} />
-              </Tab.Pane>
-              <Tab.Pane eventKey="signup">
-                <SignUpForm handleModalClose={() => setShowModal(false)} />
-              </Tab.Pane>
-            </Tab.Content>
-          </Modal.Body>
-        </Tab.Container>
-      </Modal>
-    </>
-
-      // <>
-      //   <Link className="btn btn-lg btn-info m-2" to="/login">
-      //     Login
-      //   </Link>
-      //   <Link className="btn btn-lg btn-light m-2" to="/signup">
-      //     Signup
-      //   </Link>
-      // </>
-    )}
-  </div>
-
+      {Auth.loggedIn() ? (
+        <>
+          <Button variant="danger" onClick={logout}>
+            Logout
+          </Button>
+          <span> </span>
+          <span className="text-success">
+            Welcome Back {Auth.getProfile().data.username}!! - Post or find a
+            new Challenge.
+          </span>
+        </>
+      ) : (
+        <>
+          <Button variant="primary" onClick={() => setShowModal(true)}>
+            Login/Sign Up
+          </Button>
+          <Modal
+            size="lg"
+            show={showModal}
+            onHide={() => setShowModal(false)}
+            aria-labelledby="signup-modal"
+          >
+            {/* tab container to do either signup or login component */}
+            <Tab.Container defaultActiveKey="login">
+              <Modal.Header closeButton>
+                <Modal.Title id="signup-modal">
+                  <Nav variant="pills">
+                    <Nav.Item>
+                      <Nav.Link eventKey="login">Login</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="signup">Sign Up</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                </Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <Tab.Content>
+                  <Tab.Pane eventKey="login">
+                    <LoginForm handleModalClose={() => setShowModal(false)} />
+                  </Tab.Pane>
+                  <Tab.Pane eventKey="signup">
+                    <SignUpForm handleModalClose={() => setShowModal(false)} />
+                  </Tab.Pane>
+                </Tab.Content>
+              </Modal.Body>
+            </Tab.Container>
+          </Modal>
+        </>
+      )}
+    </div>
   );
-
-  // return(
-  //     <h1>Hello login</h1>
-  // )
 };
 
 export default LoginLink;
--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,60 +1,13 @@
 import React from 'react';
-// import React, { useState } from 'react';
-// import { Link } from 'react-router-dom';
-// import {Nav, Modal, Tab } from 'react-bootstrap';
 import LoginLink from '../components/LoginLink'
-// import SignUpForm from '../components/SignupForm';
-// import LoginForm from '../components/LoginForm';
 
 const Login = () => {
-    // const [showModal, setShowModal] = 
-    // true; 
-    // useState(false);
-
 return(
     <div className="text-center">
     <h1>YOU MUST LOGIN OR SIGNUP</h1>
     <LoginLink />
-    {/* <Link onClick={() => setShowModal(true)}>Login/Sign Up</Link>
-    <Modal
-        size='lg'
-        show={showModal}
-        onHide={() => setShowModal(false)}
-        aria-labelledby='signup-modal'>
-        <Tab.Container defaultActiveKey='login'>
-          <Modal.Header closeButton>
-            <Modal.Title id='signup-modal'>
-              <Nav variant='pills'>
-                <Nav.Item>
-                  <Nav.Link eventKey='login'>Login</Nav.Link>
-                </Nav.Item>
-                <Nav.Item>
-                  <Nav.Link eventKey='signup'>Sign Up</Nav.Link>
-                </Nav.Item>
-              </Nav>
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <Tab.Content>
-              <Tab.Pane eventKey='login'>
-                <LoginForm handleModalClose={() => setShowModal(false)} />
-              </Tab.Pane>
-              <Tab.Pane eventKey='signup'>
-                <SignUpForm handleModalClose={() => setShowModal(false)} />
-              </Tab.Pane>
-            </Tab.Content>
-          </Modal.Body>
-        </Tab.Container>
-      </Modal> */}
-
-
-
     </div>
 )
-
-// return(
-//     <h1>Hello login</h1>
-// )
 };
 
 export default Login;
